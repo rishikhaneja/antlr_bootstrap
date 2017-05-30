c875df56ead6b2243a911d6eb83a7478ab9d93f7
Rishi Khaneja <rishi.khaneja@continental-corporation.com>
2017-05-31 00:12:10 +0800

    first

---
+++ antlr/run.bat
@@ -0,0 +1,2 @@
+java -jar antlr-4.7.jar -Dlanguage=CSharp Drink.g -visitor
+pause
---
+++ antlr_play/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Antlr4.Runtime.Misc;
+
+namespace antlr_play
+{
+    class AntlrDrinkListener : DrinkBaseListener
+    {
+        public override void EnterDrink([NotNull] DrinkParser.DrinkContext context)
+        {
+            System.Console.WriteLine("Drink = " + context.GetText());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = "a cup of tea";
+            ICharStream stream = CharStreams.fromstring(input);
+            ITokenSource lexer = new DrinkLexer(stream);
+            ITokenStream tokens = new CommonTokenStream(lexer);
+            DrinkParser parser = new DrinkParser(tokens);
+            parser.BuildParseTree = true;
+            IParseTree tree = parser.drinkSentence();
+            AntlrDrinkListener printer = new AntlrDrinkListener();
+            ParseTreeWalker.Default.Walk(printer, tree);
+            System.Console.ReadLine();
+        }
+    }
+}
